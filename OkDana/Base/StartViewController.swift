198400b7773282d7f35d89255e2cb50600ba948a
hekang1992 <hekang19921111@hotmail.com>
2025-12-13 10:29:43 +0800

    add-base-config

---
+++ OkDana/Base/StartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StartViewController.swift
+//  OkDana
+//
+//  Created by hekang on 2025/12/13.
+//
+
+import UIKit
+
+class StartViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
