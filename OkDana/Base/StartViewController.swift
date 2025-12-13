5152681eda5ba99e6e9c70be83614cedaf680561
hekang1992 <hekang19921111@hotmail.com>
2025-12-13 12:06:47 +0800

    add-launch-image

--- OkDana/Base/StartViewController.swift
+++ OkDana/Base/StartViewController.swift
@@ -6,15 +6,31 @@
 //
 
 import UIKit
+import SnapKit
 
 class StartViewController: BaseViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        NotificationCenter.default.post(name: NSNotification.Name("changeRootVc"), object: nil)
+//        NotificationCenter.default.post(name: NSNotification.Name("changeRootVc"), object: nil)
         
+        let bgView = UIView()
+        bgView.backgroundColor = UIColor.init(hex: "#57CF4C")
+        view.addSubview(bgView)
+        bgView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        let iconImageView = UIImageView()
+        iconImageView.image = UIImage(named: "login_logo_image")
+        bgView.addSubview(iconImageView)
+        iconImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.size.equalTo(CGSize(width: 72, height: 72))
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(100)
+        }
     }
     
 
